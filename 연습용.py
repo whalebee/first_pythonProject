9cec0dcdc24cad439caa62cb26bc4f3e99517d10
Whalebee <127908829+Whalebee@users.noreply.github.com>
2023-06-02 04:56:20 +0000

    06-02 5교시

--- 연습용.py
+++ 연습용.py
@@ -1,16 +1,20 @@
 print(" ")
 print(" ")
 
+meter_list = [1,3,5,7]
+cm_list = [100 * i for i in meter_list]
+print(cm_list)
 
 
+list_a = [1,2,3,4,5]
+result = []
+for x in list_a:
+    result.append(x*x)
+print(f"리스트 함수 사용 : {result}")
 
 
-
-
-
-
-
-
+result = [i * i for i in list_a]
+print(f"반복문 사용해서 한 번에 : {result}")
 
 
 
