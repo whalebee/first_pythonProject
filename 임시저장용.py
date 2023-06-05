39f38f385c98635231bea653536c0b47512a205a
Whalebee <127908829+Whalebee@users.noreply.github.com>
2023-06-05 07:01:29 +0000

    06-05 7교시

--- 임시저장용.py
+++ 임시저장용.py
@@ -2,7 +2,6 @@ print(" ")
 print(" ")
 
 # ex1
-
 # 함수 정의
 def ex1(*args) :
     result = 0
@@ -67,28 +66,59 @@ print(f"4번문제 나머지는 : {v2}")
 
 
 # ex5
-def palindrome(str_a):
-    str_a = str_a.replace(" ", "")
-    count = 0
-    p = 0
-    m = -1
-    for v in str_a:
-        if str_a[p] == str_a[m]:
-            count += 1
-        # print(str_a[p])
-        # print(str_a[m])
-        p += 1
-        m -= 1
-    if ( count == len(str_a)):
-        return True
-    else:
-        return False
+# 재귀함수 아닌 버전
+# def palindrome(str_a):
+#     str_a = str_a.replace(" ", "") # 공백 제거 ( 띄어쓰기 때문에 )
+#     count = 0
+#     p = 0
+#     m = -1
+#     for v in str_a:
+#         if str_a[p] == str_a[m]:
+#             count += 1
+#         # print(str_a[p])
+#         # print(str_a[m])
+#         p += 1
+#         m -= 1
+#     if ( count == len(str_a)):
+#         return True
+#     else:
+#         return False
         
+# 재귀함수 버전 -> 맞는데 최적화 필요
+# def palindrome(str_a):
+#     str_a = str_a.replace(" ","") 
+
+#     def palindrome_re(str_a, first, last):
+#         if first >= last:
+#             return True
+#         elif str_a[first] != str_a[last]:
+#             return False
+#         else:
+#             return palindrome_re(str_a, first + 1, last - 1)
+
+#     return palindrome_re(str_a, 0, len(str_a) - 1) # 인덱스의 범위때문에 -1 해줘야됨
+        
+
+def palindrome(str_a):
+    # str_a = str_a.replace(" ","")
+    print(str_a[1:-1])
+    if len(str_a) <= 1:
+        return True
+    if str_a[0] != str_a[-1]:
+        return False
+    
+    return palindrome(str_a[1:-1])
+
+
+
+
+
 print(palindrome("역삼역")) # True
 print(palindrome("12345678987654321")) # True
 print(palindrome("123454231")) # False
 print(palindrome("repaper")) # True
 print(palindrome("다리 그리고 저고리 그리다")) # True
+print(palindrome("다리 그리고 저고라 그리다")) # False
 print(palindrome("1 2 321")) # True
 print(palindrome("was it a cat i saw")) # True
 
