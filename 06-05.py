28830af94ec792325920c5617e20ede850362a60
Whalebee <127908829+Whalebee@users.noreply.github.com>
2023-06-05 08:32:47 +0000

    06-05 마지막교시

--- 06-05.py
+++ 06-05.py
@@ -159,6 +159,244 @@ print(" ")
 
 
 
+#region 함수 정의 예제
+print(" ")
+print(" ")
+
+# ex1
+# 함수 정의
+def ex1(*args) :
+    result = 0
+    for value in args:
+        result += value
+
+    return result
+
+# 매개변수 설정
+ex1_sum = ex1(3, 5, 8)
+
+# 결과 확인
+print(f"1번문제 : {ex1_sum}")
+
+
+
+
+# ex2 -> 내장함수 사용하여 최대값, 최소값 구하기
+list_ex2 = [52,273,103,32,57]
+# print(min.__doc__)
+print(f"2번문제 최소값 : {min(list_ex2)}")
+print(f"2번문제 최대값 : {max(list_ex2)}")
+
+
+
+
+# ex3
+def custom_min(*args):
+    min = args[0]
+    for x in args:
+        if x < min:
+            min = x
+    return min
+
+list_3_min = custom_min(10, 30, 40 ,50 ,60 ,70, 3, -5)
+print(f"3번문제 커스텀 최소값 : {list_3_min}")
+
+def custom_max(*args):
+    max = 0
+    for x in args:
+        if x > max:
+            max = x
+    return max
+list_3_max = custom_max(10, 30, 40, 50, 60, 70)
+print(f"3번문제 커스텀 최대값 : {list_3_max}")
+
+
+
+# ex4
+def divide(x, y):
+    quotient = x // y
+    remainder = x % y
+    # print(f"{x} // {y} = {x // y} (몫)") # 몫
+    # print(f"{x} % {y} = {x % y} (나머지)") # 나머지
+    return quotient, remainder
+# print(f"4번문제 : {divide(10, 6)}")
+v1, v2 = divide(10, 6)
+print(f"4번문제 몫은 : {v1}")
+print(f"4번문제 나머지는 : {v2}")
+
+
+
+
+# ex5
+# 재귀함수 아닌 버전
+# def palindrome(str_a):
+#     str_a = str_a.replace(" ", "") # 공백 제거 ( 띄어쓰기 때문에 )
+#     count = 0
+#     p = 0
+#     m = -1
+#     for v in str_a:
+#         if str_a[p] == str_a[m]:
+#             count += 1
+#         # print(str_a[p])
+#         # print(str_a[m])
+#         p += 1
+#         m -= 1
+#     if ( count == len(str_a)):
+#         return True
+#     else:
+#         return False
+        
+# 재귀함수 버전 -> 맞는데 최적화 필요
+# def palindrome(str_a):
+#     str_a = str_a.replace(" ","") 
+
+#     def palindrome_re(str_a, first, last):
+#         if first >= last:
+#             return True
+#         elif str_a[first] != str_a[last]:
+#             return False
+#         else:
+#             return palindrome_re(str_a, first + 1, last - 1)
+
+#     return palindrome_re(str_a, 0, len(str_a) - 1) # 인덱스의 범위때문에 -1 해줘야됨
+        
+
+def palindrome(str_a):
+    # str_a = str_a.replace(" ","")
+    print(str_a[1:-1])
+    if len(str_a) <= 1:
+        return True
+    if str_a[0] != str_a[-1]:
+        return False
+    
+    return palindrome(str_a[1:-1])
+
+
+# 다른 방법예제 ( 재귀함수 X )
+# def is_palindrome(string):
+#     if string == string[::-1]:
+#         return True
+#     else:
+#         return False
+
+
+print(palindrome("역삼역")) # True
+print(palindrome("12345678987654321")) # True
+print(palindrome("123454231")) # False
+print(palindrome("repaper")) # True
+print(palindrome("다리 그리고 저고리 그리다")) # True
+print(palindrome("다리 그리고 저고라 그리다")) # False
+print(palindrome("1 2 321")) # True
+print(palindrome("was it a cat i saw")) # True
+#endregion
+
+
+
+
+
+
+
+
+#region lambda(람다) 사용
+print(" ")
+print(" ")
+
+lambda_add = lambda x,y : x+y
+print(lambda_add(10,20))
+# 30
+
+my_list=[lambda x,y : x+y, lambda x,y : x*y]
+print(my_list[0](3,4)) # 7 
+print(my_list[1](3,4)) # 12
+
+
+
+# map -> 앞에 있는 요소(함수)를 뒤에있는 애들에게 각각 적용시켜줌
+
+# lambda 형
+print(list(map(lambda x : x+10, [1,2,3]))) # [11, 12, 13]
+
+#lambda와 list 사용
+list_a = [1,2,3]
+print(list(map(lambda x : x+10, list_a))) # [11, 12, 13] 
+
+# lambda 없이
+def plus10(x):
+    return x+10
+result = list(map(plus10, [1,2,3]))
+print(result) # [11, 12, 13] 
+
+
+# 일반 append로 사용
+def c_square(list_a):
+    list_b = []
+    for i in list_a:
+        list_b.append(i*i)
+    return list_b
+result_b = [4,5,6]
+print(c_square(result_b)) # [16, 25, 36]
+
+# map 사용
+def c_square_2(target):
+        return target*target
+print(list(map(c_square_2, result_b))) # [16, 25, 36]
+
+# map과 lambda 사용
+print(list(map(lambda x : x*x, result_b))) # [16, 25, 36]
+
+
+print(" ")
+print(" ")
+#endregion
+
+
+
+
+
+
+
+
+#region filter와 lambda 사용(2)
+print(" ")
+print(" ")
+
+
+# filter
+def fn_filter(x):
+    return 5 < x < 10
+result = list(filter(fn_filter, [5,6,7,8,9,10]))
+print(result) # [6, 7, 8, 9]
+
+# lambda 식으로 바꾸면?
+result_l = list(filter(lambda x : 5<x<10, [5,6,7,8,9,10]))
+print(result_l) # [6, 7, 8, 9]
+
+
+print(list(map(lambda x : x+10, [1,2,3])))
+# [11, 12, 13]
+
+print(list(map(lambda x: str(x) if x%2==0 else x, [1,2,3])))
+# [1, '2', 3]
+
+
+
+
+
+print(" ")
+print(" ")
+
+
+#endregion
+
+
+
+
+
+
+
+
+
+
 
 
 print(" ")
