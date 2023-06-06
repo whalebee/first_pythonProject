e46b98f1b894de1e4f2801e922cc04daabfb1e73
Whalebee <127908829+Whalebee@users.noreply.github.com>
2023-06-06 09:07:47 +0000

    06-06

--- 05-31_basic.py
+++ 05-31_basic.py
@@ -17,7 +17,8 @@ test3 = "%8d \nx %6d\n--------\n%8d" % ( a, b, a*b)
 
 # "{2}의 {0}점수는 {1}점 입니다." .format('C', 50, "Python")
 
-
+a = 2
+b = 13
 test3 = "{0:>8} \nx {1:>6}\n--------\n{2:>8}" .format( a, b, a*b )
 # print(test3)
 
