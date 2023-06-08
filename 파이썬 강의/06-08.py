b35f7321e5312471f6dba387af16b215ae5f355c
whalebee <islhoeve@gmail.com>
2023-06-08 17:30:54 +0900

    06-08 마지막교시

---
+++ 파이썬 강의/06-08.py
@@ -0,0 +1,412 @@
+print(" ")
+print(" ")
+#region 클래스 응용 예제
+
+# ex1 -> 음수 체크 해야됨
+class Person:
+    def __init__(self, name, phone, gender="N", age = 0):
+        self.name = name
+        self.__age = age
+        self.phone = phone
+        self.gender = gender
+        
+    def __str__(self):
+        return f"이름:{self.name}, 나이:{self.__age}, 전화번호:{self.phone}, 성별:{self.gender}"
+    
+    # def age_start_end(value):
+    #     def wrapper(self, age):
+    #         print(value.__name__, "함수 시작")
+    #         if age < 0:
+    #             print("입력 값 오류")
+    #             return
+    #         result = value(self, age)
+    #         print(f"입력된 나이 : {result}")
+    #         print(value.__name__, "함수 정상 종료")
+    #         return result
+    #     return wrapper
+    
+    # @age_start_end
+    # age = property(get_age, set_age)
+    # 2번째 방법으로 하라는 뜻
+    @property
+    def age(self):
+        return self.__age
+    
+    @age.setter
+    def age(self, age):
+        if age < 0:
+            print("입력 값 오류")
+            return
+        self.__age = age # 0 보다 크면 여기와서 저장
+    
+    
+
+    def greeting(self):
+        print(f"안녕하세요. {self.name}입니다.")
+
+    def add_age(self):
+        if(self.__age == 0):
+            print("나이가 입력되어있지 않습니다.")
+            return
+        else:
+            print(f"나이 추가 : {self.__age} + 1 = {self.__age + 1}")
+            self.__age += 1
+
+    def change_phone(self, new_number):
+        self.phone = new_number
+
+
+yone = Person("yonezu kenshi", "010-1234-1234", "M")
+yone.age = 30
+print(f"나이 입력 후 : {yone}")
+
+yone.greeting()
+print(f"인사 후 : {yone}")
+
+yone.add_age()
+print(f"나이 추가 후 : {yone}")
+
+yone.change_phone("010-5555-5555")
+print(f"번호 교체 후 : {yone}")
+
+print(" ")
+print(" ")
+
+
+
+# ex2
+class FourCalc:
+    v1 = 0
+    v2 = 0
+
+    def setdata(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+
+    def add(self):
+        return self.v1 + self.v2
+    
+    def sub(self):
+        return self.v1 - self.v2
+    
+    def mul(self):
+        return self.v1 * self.v2
+    
+    def div(self):
+        return self.v1 / self.v2
+    
+
+
+ex2 = FourCalc()
+ex2.setdata(15, 5)
+print(f"더하기 : {ex2.add()}")
+print(f"빼기   : {ex2.sub()}")
+print(f"곱하기 : {ex2.mul()}")
+print(f"나누기 : {ex2.div()}")
+
+
+
+# ex3
+# 탈락 버전
+# class Calculator:
+#     def __init__(self, list):
+#         self.list = list
+#         self.result_sum = 0
+            
+#     def sum(self):
+#         for value in self.list:
+#             self.result_sum += value
+#         return self.result_sum
+    
+#     def avg(self):
+#         count = 0
+#         for value in self.list:
+#             count += 1
+#         return self.result_sum / count
+
+
+# cal1 = Calculator([1,2,3,4,5])
+# print(cal1.sum())
+# print(cal1.avg())
+
+# cal2 = Calculator([6,7,8,9,10])
+# print(cal2.sum())
+# print(cal2.avg())
+
+# ex3 합격 버전
+class Calculator:
+    def __init__(self, list):
+        self.list = list
+
+            
+    def sum(self):
+        result_sum = 0
+        for value in self.list:
+            result_sum += value
+        return result_sum
+    
+    def avg(self):
+        call_sum = sum(self.list)
+        count = len(self.list)
+        return call_sum / count
+
+
+cal1 = Calculator([1,2,3,4,5])
+# print(cal1.sum())
+print(cal1.avg())
+
+cal2 = Calculator([6,7,8,9,10])
+# print(cal2.sum())
+print(cal2.avg())
+
+
+
+
+
+print(" ")
+print(" ")
+
+
+
+#endregion
+
+
+
+
+
+
+
+
+#region 클래스의 상속
+print(" ")
+print(" ")
+
+class Person:
+    def greeting(self):
+        print("하이 ~")
+
+class Grade:
+    def greeting(self):
+        print("Grade입니다 하이 ~")
+    def score(self):
+        print("학점을 관리합니다.")
+
+
+# Student 클래스가 Person 이라는 클래스를 상속받음
+# Person, Grade로 선언이 되어있으니
+# 중복된 메서드 중에서 본인 클래스가 가장 우선 순위를 가지고
+# 그 다음은 선언된 순서로 가진다 ( ex. Person (1) -> Grade (2) )
+# 그래서 밑에 예시에서 greeting은 Person클래스에 있는 greeting 메서드를 쓰게 된다.
+class Student(Person, Grade):
+    def study(self):
+        print("공부하세요 경준이 !")
+
+    # 부모 클래스가 가지고 있는 것과 같은게 있으면
+    # 해당 클래스 안에 있는 것이 더 우선 순위를 가짐
+    # def greeting(self):
+    #     print("Hello")
+
+    def greeting(self):
+        super().greeting()  # 이것도 쓰고
+        Grade.greeting(self)# 이것도 쓰고
+        print("Hello")      # 이것도 쓰고ㅋㅋ
+    # 이렇게 하면 3개를 다 쓰는 것
+
+lee = Student()
+lee.study()
+lee.greeting() # 이렇게 상속받아서 greeting 메서드가 사용가능해짐
+lee.score()
+lee.study()
+
+
+print(issubclass(Student, Person)) # True
+
+
+
+
+
+
+
+
+
+print(" ")
+print(" ")
+
+
+#endregion
+
+
+
+
+
+
+
+
+#region 클래스 상속 예제1,2번
+
+# ex1
+class Person:
+    def __init__(self, name, phone, gender="N", age = 0):
+        self.name = name
+        self.__age = age
+        self.phone = phone
+        self.gender = gender
+        
+    def __str__(self):
+        return f"이름:{self.name}, 나이:{self.__age}, 전화번호:{self.phone}, 성별:{self.gender}"
+    
+    # def age_start_end(value):
+    #     def wrapper(self, age):
+    #         print(value.__name__, "함수 시작")
+    #         if age < 0:
+    #             print("입력 값 오류")
+    #             return
+    #         result = value(self, age)
+    #         print(f"입력된 나이 : {result}")
+    #         print(value.__name__, "함수 정상 종료")
+    #         return result
+    #     return wrapper
+    
+    # @age_start_end
+    # age = property(get_age, set_age)
+    # 2번째 방법으로 하라는 뜻
+    @property
+    def age(self):
+        return self.__age
+    
+    @age.setter
+    def age(self, age):
+        if age < 0:
+            print("입력 값 오류")
+            return
+        self.__age = age # 0 보다 크면 여기와서 저장
+    
+    def greeting(self):
+        print(f"안녕하세요. {self.name}입니다.")
+
+    def add_age(self):
+        if(self.__age == 0):
+            print("나이가 입력되어있지 않습니다.")
+            return
+        else:
+            print(f"나이 추가 : {self.__age} + 1 = {self.__age + 1}")
+            self.__age += 1
+
+    def change_phone(self, new_number):
+        self.phone = new_number
+
+
+class Student(Person):
+    def __init__(self, name, phone, gender="N", age=0):
+        super().__init__(name, phone, gender, age)
+        self.score = {}
+        self.result_total = 0
+        self.call_total = 0
+        # self.iscall_total = 0
+        self.result_avg = 0
+
+    def __str__(self):
+        print(f"학생정보 : {super().__str__()}")
+        return f"성적기록 : {self.score}"
+    
+    def add_subject(self, subject, score):
+        if subject in self.score:
+            print(f"{subject} 과목의 점수 갱신 ! ")
+        self.score[subject] = score
+    
+    def total_score(self):
+        self.iscall_total = 1
+        for x,y in self.score.items():
+            self.result_total += y
+            # print(y)
+        print(f"총 점수 : {self.result_total}점")
+        # return self.total
+
+    def average_score(self):
+        if self.iscall_total == 1:
+            call_total = self.result_total
+        else:
+            call_total = self.total_score()
+        result_avg = call_total / len(self.score)
+        # print(call_total)
+        print("평균 점수는 : {0:.2f}" .format(result_avg))
+        # print(f"평균 점수는 : {result_avg:.2f}")
+
+yone = Student("yonezu kenshi", "010-1234-1234", "M")
+yone.age = 31
+yone.add_subject("C", 80)
+yone.add_subject("C", 98) # 과목 갱신
+yone.add_subject("Python", 90)
+yone.add_subject("Network", 80)
+yone.add_subject("Linux", 60)
+yone.add_subject("Maria_DB", 70)
+yone.add_subject("Maria_DB", 90) # 과목 갱신
+print(yone)
+# yone.total_score()
+yone.average_score()
+
+
+
+
+
+# ex2
+class FourCalc:
+    # v1 = 0
+    # v2 = 0
+
+    def setdata(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+
+    def add(self):
+        return self.v1 + self.v2
+    
+    def sub(self):
+        return self.v1 - self.v2
+    
+    def mul(self):
+        return self.v1 * self.v2
+    
+    def div(self):
+        return self.v1 / self.v2
+
+
+class MoreFourCalc(FourCalc):
+    def __init__(self, v1, v2):
+        self.v1 = v1 
+        self.v2 = v2
+
+    def square(self):
+        result = 1
+        for x in range(self.v2):
+            result *= self.v1
+        return result
+
+cal3 = MoreFourCalc(2, 16)
+print(cal3.square())
+print(cal3.add())
+print(cal3.sub())
+print(cal3.mul())
+print(cal3.div())
+
+
+
+
+#endregion
+
+
+
+
+
+
+
+
+
+print(" ")
+print(" ")
+
+
+
+
+
+
