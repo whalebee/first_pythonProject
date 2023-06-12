cff1656418b16d3944ef98289af441e0943c0ea1
whalebee <islhoeve@gmail.com>
2023-06-12 17:28:17 +0900

    06-12 last

--- 연습용.py
+++ 연습용.py
@@ -1,12 +1,14 @@
 print(" ")
 print(" ")
 
+# 구구단
 
-
-
-
-
-
+def pan(str_a):
+    if len(str_a) <= 1:
+        return True
+    if str_a[0] != str_a[-1]:
+        return False
+    return pan(str_a[1:-1])
 
 
 print(" ")
